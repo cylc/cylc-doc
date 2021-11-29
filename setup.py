9fd3b149df404b1d03b0110bc75d995eac53cbde
Hilary James Oliver <hilary.j.oliver@gmail.com>
2021-11-29 22:49:33 +1300

    Pin setuptools to >= 50.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ INSTALL_REQUIRES = [
     'cylc-sphinx-extensions>=1.3.3',
     'eralchemy==1.2.*',
     'hieroglyph>=2.1.0',
-    'setuptools>=49',
+    'setuptools>=50',
     'sphinx>=3.0.0',
     'sphinx_rtd_theme>=0.5.0',
     'sphinxcontrib-svg2pdfconverter',
