22f091b40beb4ab977c63e4fbaf00e60fae49b19
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2023-10-02 15:56:36 +0100

    fix

--- src/conf.py
+++ src/conf.py
@@ -150,16 +150,13 @@ linkcheck_ignore = [
     r'https://github.com/.*',
 ]
 
-user_agent = 'Mozilla/5.0'
+# user_agent = 'Mozilla/5.0'
 
-# linkcheck_request_headers = {
-#     'https://linux.die.net/man/1/bash': {
-#         'User-Agent': (
-#             'Mozilla/5.0 (X11; Linux x86_64; rv:12.0)'
-#             ' Gecko/20100101 Firefox/12.'
-#         )
-#     }
-# }
+linkcheck_request_headers = {
+    'https://linux.die.net/man/1/bash': {
+        'referer': 'Firefox'
+    }
+}
 
 nitpick_ignore_regex = [
     # intersphinx has trouble with pyzmq classes:
