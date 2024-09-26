cd67eafd65511e381a066f3e7c0bb8fe8831491e
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2024-09-26 16:12:44 +0100

    prevent example README files getting built

--- src/conf.py
+++ src/conf.py
@@ -144,7 +144,7 @@ nitpick_ignore = [
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-# exclude_patterns = ['_build']
+exclude_patterns = ['user-guide/examples/**README.rst']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'autumn'
